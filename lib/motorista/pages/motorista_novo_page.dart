1126ef1b6f9eff1c7c5670b9c71e723e15099293
Anailson Mota <mota.a.santos@gmail.com>
2022-12-05 07:33:59 -0300

    Cria tela de cadastro e detalhes do motorista

---
+++ lib/motorista/pages/motorista_novo_page.dart
@@ -0,0 +1,40 @@
+import 'package:coopertalse_motorista/carro/carro.dart';
+import 'package:coopertalse_motorista/motorista/motorista.dart';
+import 'package:coopertalse_motorista/motorista/pages/components/form_motorista.dart';
+import 'package:flutter/material.dart';
+
+class MotoristaNovoPage extends StatelessWidget {
+
+  MotoristaNovoPage({ super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    
+    Motorista? motorista = Motorista(nome: "Joaozinho", carro: Carro(20), id: 1);
+    // motorista = null;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Novo Motorista")),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Insira as informações do carro",
+                    style: TextStyle(fontSize: 18, color: Colors.black87),
+                  ),
+                ],
+              ),
+            ),
+            FormMotorista(motorista),
+          ],
+        ),
+      ),
+    );
+  }
+}
