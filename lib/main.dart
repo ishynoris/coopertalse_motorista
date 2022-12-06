329de2a01ac071ab667fb6fc9bbda481a4000338
Anailson Mota <mota.a.santos@gmail.com>
2022-12-06 20:45:09 -0300

    Renomeia arquivo motorista_novo_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:coopertalse_motorista/motorista/pages/motorista_novo_page.dart';
+import 'package:coopertalse_motorista/motorista/pages/motorista_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,7 +22,7 @@ class _AppState extends State<App> {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MotoristaNovoPage(),
+      home: MotoristaPage(),
     );
   }
 }
