e5400979b55d541d5bfcf86472b17d46306e410e
Anailson Mota <mota.a.santos@gmail.com>
2022-12-07 01:46:00 -0300

    Corrige a iniciação do SharedPreferences

--- lib/motorista/repo/shared_preferences_repo.dart
+++ lib/motorista/repo/shared_preferences_repo.dart
@@ -3,18 +3,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../dao/motorista_shared_prefs.dart';
 
 class SharedPreferencesRepo {
-  
-  static late SharedPreferences _prefs;
+
+  static late SharedPreferences prefs;
 
   static MotoristaSharedPreferences get getMotorista {
-    SharedPreferencesRepo._init();
-    return MotoristaSharedPreferences(SharedPreferencesRepo._prefs);
+    return MotoristaSharedPreferences(SharedPreferencesRepo.prefs);
   }
 
-  static Future _init() async {
-    // ignore: unnecessary_null_comparison
-    if (SharedPreferencesRepo == null) {
-      SharedPreferencesRepo._prefs = await SharedPreferences.getInstance();
-    }
-  }
+  static init() async => SharedPreferencesRepo.prefs = await SharedPreferences.getInstance();
 }
