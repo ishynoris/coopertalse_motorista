1412120248c188927c8f5123750760bfc62b7d1f
Anailson Mota <mota.a.santos@gmail.com>
2022-12-13 23:18:11 -0300

    Adiciona permissão de GPS

--- lib/dependencias/dependencias.dart
+++ lib/dependencias/dependencias.dart
@@ -1,8 +1,10 @@
 import 'package:coopertalse_motorista/motorista/repo/shared_preferences_repo.dart';
+import 'package:coopertalse_motorista/util/localizacao.dart';
 
 class Dependencias {
 
   static init() async {
     await SharedPreferencesRepo.init();
+    await Localizacao.requestGPS();
   }
 }
