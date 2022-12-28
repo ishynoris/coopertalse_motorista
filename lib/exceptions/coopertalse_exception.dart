b6c5159103535cd591e76189be4d4a95f0a2afa0
Anailson Mota <mota.a.santos@gmail.com>
2022-12-28 04:14:32 -0300

    Relança a exceção caso seja do tipo coopertalse

--- lib/exceptions/coopertalse_exception.dart
+++ lib/exceptions/coopertalse_exception.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 class CoopertalseException implements Exception {
 
   final String mensagem;
@@ -8,5 +6,17 @@ class CoopertalseException implements Exception {
   @override
   String toString() {
     return this.mensagem;
+  }
+
+  static retrows(Object e, { String padrao = "Erro inesperado"}) {
+    throw CoopertalseException._is(e) ? e : CoopertalseException(padrao);
+  } 
+
+  static String message(Object e, { String padrao = "Erro inesperado"}) {
+    return CoopertalseException._is(e) ? e.toString() : padrao;
+  } 
+
+  static bool _is(Object e) {
+    return e is CoopertalseException;
   }
 }
